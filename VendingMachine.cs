079f8ec74692bacea62ef0dc4b6e60ba82593c0d
chrisscherer <chrisscherer90@gmail.com>
2016-09-20 16:30:23 -0400

    Initial Push and project setup

---
+++ VendingMachine.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VendingMachineKata
+{
+	class VendingMachine
+	{
+
+	}
+}
---
+++ VendingMachineTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace VendingMachineKata
+{
+	[TestFixture ()]
+	public class VendingMachineTests
+	{
+		VendingMachine v;
+
+		[SetUp] public void Init ()
+		{
+			v = new VendingMachine ();
+		}
+
+		[TearDown] public void Dispose ()
+		{
+			v = null;
+		}
+
+		[Test ()]
+		public void when ()
+		{
+			Assert.AreEqual (0, 0);
+		}
+	}
+}
